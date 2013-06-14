c08953d6ab7b32cc2cf50048bda9b1008794bff2
newdazhu <newdazhu@sina.com>
2013-06-14 22:48:02 +0800

    + add boost 1.53

--- study/aboutboost/test/test/algorithm_boyermoore.cpp
+++ study/aboutboost/test/test/algorithm_boyermoore.cpp
@@ -5,17 +5,19 @@
 using namespace std;
 int main()
 {
-    string strPatt = "abcdcba";
+    string strPatt = "aaabbcdefedaa";
     string strBuff = "ajdabcdcbancaidabcdceqonfkabcasnafuababgjsabcdcbakhf";
     string strBuff2 = "ajdabcdcbncaidabcdceqonfkabcasnafuababgjsabcdcbakhf";
     // 1 boyer_moore
     boost::algorithm::boyer_moore<string::iterator> searchObj(strPatt.begin(), strPatt.end());
     string::iterator it = searchObj(strBuff.begin(), strBuff.end());
+    cout << *it << endl;
 
     it = boost::algorithm::boyer_moore_search(strBuff2.begin(), strBuff2.end(), strPatt.begin(), strPatt.end());
-    
+    cout << *it << endl;
     
 
+    
 
 
 
